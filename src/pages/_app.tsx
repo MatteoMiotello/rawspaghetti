617b75db2653f640aa6495903a1ad56d80624b64
leonardodbn <leonardodeboni98@gmail.com>
2023-04-01 20:05:06 +0200

    added importsd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
-import '@/styles/globals.css'
-import type {AppProps} from 'next/app'
-import {Navbar} from "@/components/navbar";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Navbar } from '@/components/navbar';
+import React from 'react';
 
-export default function App({Component, pageProps}: AppProps) {
-    return <div>
-        <Navbar></Navbar>
-        <Component {...pageProps} />
-    </div>
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<div>
+			<Navbar></Navbar>
+			<Component {...pageProps} />
+		</div>
+	);
 }
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,79 +1,79 @@
-import {GetStaticProps, NextPage} from "next";
-import Head from "next/head";
-import {collection} from "@/lib/collection/Collection";
-import moment from "moment";
-import {Post} from "@/lib/collection/entity/Post";
-import {postDef} from "@/lib/collection/entity";
+import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import { collection } from '@/lib/collection/Collection';
+import moment from 'moment';
+import { Post } from '@/lib/collection/entity/Post';
+import { postDef } from '@/lib/collection/entity';
+import React from 'react';
 
 interface PostProps {
-    content: string,
-    title: string,
-    author: string,
-    category: string
-    date: string
-    error: string | null
-
+	content: string;
+	title: string;
+	author: string;
+	category: string;
+	date: string;
+	error: string | null;
 }
 
 const PostPage: NextPage = (props: PostProps) => {
-    if (props.error != null) {
-        return <p>Not found { props.error }</p> //todo make it better
-    }
+	if (props.error != null) {
+		return <p>Not found {props.error}</p>; //todo make it better
+	}
 
-    return <>
-        <Head>
-            <title>{props.title}</title>
-        </Head>
-        <div className="w-full">
-            <article className="prose prose-slate md:prose-lg lg:prose-2xl mx-auto">
-                <h1>{props.title}</h1>
-                <div dangerouslySetInnerHTML={{__html: props.content}}></div>
-                <hr></hr>
-                <p>Article by {props.author}</p>
-            </article>
-
-        </div>
-    </>
-}
+	return (
+		<>
+			<Head>
+				<title>{props.title}</title>
+			</Head>
+			<div className="w-full">
+				<article className="prose prose-slate md:prose-lg lg:prose-2xl mx-auto">
+					<h1>{props.title}</h1>
+					<div
+						dangerouslySetInnerHTML={{ __html: props.content }}
+					></div>
+					<hr></hr>
+					<p>Article by {props.author}</p>
+				</article>
+			</div>
+		</>
+	);
+};
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    const slug = String(context.params.slug);
+	const slug = String(context.params.slug);
 
-    const post = collection<Post>(postDef)
-        .filter('name', slug)
-        .getFirst()
+	const post = collection<Post>(postDef).filter('name', slug).getFirst();
 
-    if (post == null) {
-        return {
-            props: {
-                error: 'post not found'
-            }
-        }
-    }
+	if (post == null) {
+		return {
+			props: {
+				error: 'post not found',
+			},
+		};
+	}
 
-    return {
-        props: {
-            content: post.content,
-            title: post.title,
-            author: post.author,
-            category: post.category,
-            date: moment(post.date).format('d/M/Y')
-        }
-    }
-}
+	return {
+		props: {
+			content: post.content,
+			title: post.title,
+			author: post.author,
+			category: post.category,
+			date: moment(post.date).format('d/M/Y'),
+		},
+	};
+};
 
 export const getStaticPaths = async (p: any) => {
-    const postCollection = collection<Post>(postDef).getAll()
+	const postCollection = collection<Post>(postDef).getAll();
 
-    return {
-        paths: postCollection.map((post: Post) => ({
-                params: {
-                    slug: post.name
-                }
-            })
-        ),
-        fallback: false
-    }
-}
+	return {
+		paths: postCollection.map((post: Post) => ({
+			params: {
+				slug: post.name,
+			},
+		})),
+		fallback: false,
+	};
+};
 
-export default PostPage
+export default PostPage;
